999d23be75fdab077483bc0dadd94fc7afcf404f
suyeon Park <sypark180@gmail.com>
2025-06-10 06:48:14 +0000

    inventory 추가

--- product/src/main/java/stmall/domain/Inventory.java
+++ product/src/main/java/stmall/domain/Inventory.java
@@ -45,19 +45,20 @@ public class Inventory {
         stockDecreased.publishAfterCommit();
         */
 
-        /** Example 2:  finding and process
+        /** Example 2:  finding and process */
         
 
-        repository().findById(deliveryCompleted.get???()).ifPresent(inventory->{
+        repository().findById(Long.valueOf(deliveryCompleted.getItemId())).
+        ifPresent(inventory->{
             
-            inventory // do something
+            inventory.setStock(inventory.getStock() - deliveryCompleted.getQty()); // do something
             repository().save(inventory);
 
             StockDecreased stockDecreased = new StockDecreased(inventory);
             stockDecreased.publishAfterCommit();
 
          });
-        */
+        
 
     }
 
